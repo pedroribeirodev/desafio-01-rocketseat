ef4ad3be2a41b09129298bcc4d391ff294f73c30
Pedro Henrique Ribeiro <pedro.bonfim.13@gmail.com>
2019-08-24 11:31:18 -0300

    fix - ajustes no numero da porta

--- index.js
+++ index.js
@@ -4,19 +4,9 @@ const server = express();
 
 server.use(express.json());
 
-/**
- * Utilizamos a variável `numberOfRequests` como
- * `let` porque vai sofrer mutação. A variável
- * `projects` pode ser `const` porque um `array`
- * pode receber adições ou exclusões mesmo sendo
- * uma constante.
- */
 let numberOfRequests = 0;
 const projects = [];
 
-/**
- * Middleware que checa se o projeto existe
- */
 function checkProjectExists(req, res, next) {
   const { id } = req.params;
   const project = projects.find(p => p.id == id);
@@ -28,9 +18,6 @@ function checkProjectExists(req, res, next) {
   return next();
 }
 
-/**
- * Middleware que dá log no número de requisições
- */
 function logRequests(req, res, next) {
   numberOfRequests++;
 
@@ -41,9 +28,6 @@ function logRequests(req, res, next) {
 
 server.use(logRequests);
 
-/**
- * Projects
- */
 server.get("/projects", (req, res) => {
   return res.json(projects);
 });
@@ -83,9 +67,6 @@ server.delete("/projects/:id", checkProjectExists, (req, res) => {
   return res.send();
 });
 
-/**
- * Tasks
- */
 server.post("/projects/:id/tasks", checkProjectExists, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
@@ -97,4 +78,4 @@ server.post("/projects/:id/tasks", checkProjectExists, (req, res) => {
   return res.json(project);
 });
 
-server.listen(4000);
+server.listen(3333);
